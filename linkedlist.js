921fb2278ef8242d991a02fbd5d260ab8572e6d4
Raam <v.raamaanujam@gmail.com>
2023-02-27 19:38:04 +0530

    First

--- linkedlist.js
+++ linkedlist.js
@@ -1,3 +1,4 @@
+/*
 function LinkedList() { 
     var length = 0; 
     var head = null; 
@@ -139,3 +140,133 @@ function LinkedList() {
   console.log(conga.elementAt(3));
   console.log(conga.indexOf('Puppy'));
   console.log(conga.size());
+  */
+
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  InsertFirst(data) {
+    this.head = new Node(data, this.head);
+    this.size++;
+  }
+
+  InsertLast(data) {
+    let node = new Node(data);
+    let current;
+
+    if (!this.head) {
+      this.head = node;
+    } else {
+      current = this.head;
+
+      while (current.next) {
+        current = current.next;
+      }
+
+      current.next = node;
+    }
+
+    this.size++;
+  }
+
+  InsertAt(data, index) {
+    if (index > 0 && index > this.size) {
+      return;
+    }
+
+    if (index === 0) {
+      this.InsertFirst(data);
+      return;
+    }
+
+    const node = new Node(data);
+    let current, previous;
+
+    current = this.head;
+    let count = 0;
+
+    while (count < index) {
+      previous = current;
+      count++;
+      current = current.next;
+    }
+
+    node.next = current;
+    previous.next = node;
+
+    this.size++;
+  }
+
+  GetAt(index) {
+    let current = this.head;
+    let count = 0;
+
+    while (current) {
+      if (count == index) {
+        console.log(current.data);
+      }
+      count++;
+      current = current.next;
+    }
+
+    return null;
+  }
+
+  RemoveAt(index) {
+    if (index > 0 && index > this.size) {
+      return;
+    }
+
+    let current = this.head;
+    let previous;
+    let count = 0;
+
+    if (index === 0) {
+      this.head = current.next;
+    } else {
+      while (count < index) {
+        count++;
+        previous = current;
+        current = current.next;
+      }
+
+      previous.next = current.next;
+    }
+
+    this.size--;
+  }
+
+  ClearList() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  PrintListData() {
+    let current = this.head;
+
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
+
+const l = new LinkedList();
+
+l.InsertFirst(100);
+l.InsertFirst(200);
+l.InsertFirst(300);
+l.InsertLast(400);
+l.InsertAt(500, 3);
+
+l.PrintListData();

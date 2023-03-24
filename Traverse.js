6bedbf3a49f039fcfb851911df082dc4c55dabf8
Raam <v.raamaanujam@gmail.com>
2023-03-24 19:16:37 +0530

    Problem 1

---
+++ Traverse.js
@@ -0,0 +1,91 @@
+class Node
+{
+    constructor(data)
+    {
+        this.prev = null;
+        this.value = data;
+        this.next = null;
+    }
+}
+
+class LinkedList
+{
+    constructor()
+    {
+        this.head = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    Insert(data)
+    {
+        let node = new Node(data);
+        if(this.head == null)
+        {
+            this.head = node;
+            this.last = node;
+        }
+        else
+        {
+            this.last.next = node;
+            node.prev = this.last;
+            this.last = node;
+        }
+        this.size++;
+    }
+
+    
+    PrintList()
+    {
+        let current = this.head;
+        while(current)
+        {
+            console.log(current.value);
+            current = current.next;
+        }
+    }
+
+    ReversePrint()
+    {
+        let current = this.last;
+        while(current)
+        {
+            console.log(current.value);
+            current = current.prev;
+        }
+    }
+
+    Traverse()
+    {
+        let num = 632,count=0;
+        let current = this.head;
+        let rem = num%10;
+        while(current)
+        {
+            current = current.next;
+            count++;
+            if(current.value == rem)
+            {
+                count = 0;
+            }
+        }
+    }
+  
+}
+
+// const PrintData = (head) =>{
+//     if(head == null) return;
+//     console.log(head.value);
+//     PrintData(head.next);
+//   }
+
+const ll = new LinkedList();
+ll.Insert(1);
+ll.Insert(2);
+ll.Insert(3);
+ll.Insert(4);
+ll.Insert(5);
+ll.Insert(6);
+ll.PrintList();
+console.log("---")
+ll.ReversePrint();
